e3dd8c5dcfc28f2edfe6e0d7da7bf59078c9c1fe
Sex thủ si tềnh <khoa.ngkhoa26@gmail.com>
2023-08-02 15:32:41 +0700

    Done Textview SeeAll ChangeFragment and BottomNav

--- app/src/main/java/com/example/android_app_shop/View/FragHome.java
+++ app/src/main/java/com/example/android_app_shop/View/FragHome.java
@@ -1,12 +1,17 @@
 package com.example.android_app_shop.View;
 
+import android.annotation.SuppressLint;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -18,6 +23,7 @@ import com.example.android_app_shop.Model.PagerImageProductDetail;
 import com.example.android_app_shop.Model.Product;
 import com.example.android_app_shop.Model.ProductShowInDetailAdapter;
 import com.example.android_app_shop.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +35,12 @@ public class FragHome extends Fragment {
     ProductHandlder productHandlder;
     ImageProductHandler imageProductHandler;
     ArrayList<Product> productArrayList = new ArrayList<>();
+
+    BottomNavigationView bottomNavigationView;
+
+    TextView SeeAll ;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,18 +55,34 @@ public class FragHome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.frag_home, container, false);
+
+        SeeAll = view.findViewById(R.id.TV_SeeAll);
         ListProductHomePage = (GridView) view.findViewById(R.id.gridProductHomePage);
+        bottomNavigationView = getActivity().findViewById(R.id.bottom_nav);
+
         productHandlder = new ProductHandlder(getContext(), "SMARTPHONE.db", null, 1);
         imageProductHandler = new ImageProductHandler(getContext(), "SMARTPHONE.db", null, 1);
+
         imageProductHandler.initData();
         productHandlder.initData();
         productArrayList = productHandlder.loadProduct();
+
         addEvents();
         showProduct();
         return view;
     }
 
     private void addEvents(){
+
+        SeeAll.setPaintFlags(SeeAll.getPaintFlags() |   Paint.UNDERLINE_TEXT_FLAG);
+
+        SeeAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomNavigationView.setSelectedItemId(R.id.navigation_shop);
+            }
+        });
+
         ListProductHomePage.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
